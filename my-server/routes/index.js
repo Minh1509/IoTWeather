5d3e78ac09edd4707faeef33b6266d5bec2a562b
Minh1509 <nguyenquangminh15092003@gmail.com>
2024-09-25 13:36:09 +0700

    done

---
+++ my-server/routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const Controller = require("../controllers/controller");
+
+
+router.get("/datasensor", Controller.DataSensor);
+router.get("/action_history", Controller.DataHistory);
+router.post("/publish", Controller.PublishData);
+
+module.exports = router;
